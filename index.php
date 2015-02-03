a7c9f721229e0d45098406191b09754d206143fa
MistSF <kevin@faby.fr>
2015-02-03 17:07:37 +0100

    Revert "debut"
    This reverts commit 60dc7d4257e20bffd8fa58aa50a87abad663bf91.

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPe html>
-<html>
-	<head>
-	</head>
-	<body>
-		<?php include('test.php');
-		recup();?>
-	</body>
-</html>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-function recup()
-{
-	$var = simplexml_load_file('20141010_ExpHadopiMetadonneesVOD.xml');
-	return $var;
-}
