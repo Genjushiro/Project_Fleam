b48d1d53521e545813ec87fd77d67f469d25d4dd
geektaku <jalet_y@etna-alternance.net>
2015-02-04 18:03:46 +0100

    cast
    cast

--- Project_f/fiche.php
+++ Project_f/fiche.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+.<!DOCTYPE html>
 <html>
 	<head>
 		<?php include_once("head.php");?>
@@ -15,8 +15,32 @@
 	function aff_film($val){
 		$obj = open_file();
 		$f = 0;
+		$verif_tab = array();
 		foreach($obj as $elem)
 		{
+		$txt = (string) $elem->Title;
+			if (!verif($verif_tab, $txt))
+			{
+				if ($val == null || $val == $elem->{'Title'} || $val == $elem->{'Director'})	
+					$ok = 1;
+				else
+				{
+					$actor = (array) $elem->Cast;
+					$key = (array) $elem->Keywords;
+					if ((isset($actor['Actor']) && $actor['Actor'] == $val) || (isset($key['Keyword']) && $key == $val))
+						$ok = 1;
+					else 
+					{
+						if (isset($actor['Actor'][0]))
+							for ($i = 0; isset($actor['Actor'][$i]); $i++)
+								if ($actor['Actor'][$i] == $val)
+									$ok = 1;
+						if (isset($key['Keyword'][0]))
+							for ($i = 0; isset($key['Keyword'][$i]); $i++)
+								if ($key['Keyword'][$i] == $val)
+									$ok = 1;
+				}
+			}
 			if (($val == null || $val == $elem->{'Title'}) && $f == 0)
 			{
 				echo $elem->{'Title'};
