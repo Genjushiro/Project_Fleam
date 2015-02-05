35004039f70945be8841df35e8fe9389c9f3fdae
geektaku <jalet_y@etna-alternance.net>
2015-02-05 19:58:08 +0100

    KATEGORY

--- Project_f/fiche.php
+++ Project_f/fiche.php
@@ -17,32 +17,38 @@
 		$f = 0;
 		foreach($obj as $elem)
 		{
+			if ($val == null || $val == $elem->{'Title'} || $val == $elem->{'Director'})
+				{
+					$actor = (array) $elem->Cast;
+					$key = (array) $elem->Keywords;
+					$cat = (array) $elem->Categories; 
+					$ok = 1;
+				}
 			if (($val == null || $val == $elem->{'Title'}) && $f == 0)
 			{
-				echo $elem->{'Title'};
-				echo "<br/>";
-				echo $elem->{'OriginalTitle'};
-				echo "<br/>";
-				echo $elem->{'PublicationDate'};
-				echo "<br/>";
-				echo "duree:" . $elem->{'Duration'} . "min";
-				echo "<br/>";
-				echo $elem->{'Director'};
-				echo "<br/><br/>";
-				echo $elem->{'Plot'};
-				echo "<br/><br/>";
-				echo $elem->{'Cast'};
-				echo "<br/><br/>";
-				echo $elem->{'ParentalControl'};
-				echo "<br/>";
+				echo "titre: ",$elem->{'Title'},"<br/>";
+				echo "titre originale: ",$elem->{'OriginalTitle'},"<br/>";
+				echo "realisateur: ",$elem->{'Director'},"<br/>";
+				echo "date de sortie:",$elem->{'PublicationDate'},"<br/>";
+				echo "duree: ",$elem->{'Duration'},"<br/>";
+				echo "Langage: ",$elem->{'Language'},"<br/>";
+				echo "Qualite d'image: ",$elem->{'ImageQuality'},"<br/><br/>";
+				echo "synopsis: ",$elem->{'Plot'},"<br/><br/>";
+				echo "audience: ",$elem->{'ParentalControl'},"<br/><br/>";
+				echo "casting:","<br/>";
+				for ($i = 0; isset($actor['Actor'][$i]); $i++)
+					echo $actor['Actor'][$i] , "<br/>";
+				echo "<br/>" , "mot cle:" , "<br/>";
+				for ($i = 0; isset($key['Keyword'][$i]); $i++)
+					echo $key['Keyword'][$i] , "<br/>";
+				echo "Categories: ";
+				for ($i = 0; isset($cat['Category'][$i]); $i++)
+					echo $cat['Category'][$i] , "<br/>";
 				$f = 1;
 			}
 			if ($val == null || $val == $elem->{'Title'})
 			{
-				echo $elem->{'VODPlatform'};
-				echo "<br/>";
-				echo $elem->{'URL'};
-				echo "<br/>";
+				echo "<br/>","disponible sur:","<br/>",'<a href="' . $elem->{'URL'} . '">' . $elem->{'VODPlatform'} . '</a>',"<br/>";
 			}
 		}
 	}
